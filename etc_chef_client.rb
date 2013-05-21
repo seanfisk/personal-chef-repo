6e6d89252031430ca49400bb812a1ad6b09999a2
Sean Fisk <sean@seanfisk.com>
2013-05-21 10:21:44 -0600

    Add an example /etc/chef/client.rb file.

---
+++ etc_chef_client.rb
@@ -0,0 +1,11 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://api.opscode.com/organizations/sean_fisk'
+validation_client_name 'sean_fisk-validator'
+
+# make runnable by standard user
+base_dir = '/Users/sean/.chef'
+checksum_path           "#{base_dir}/checksum"
+file_cache_path         "#{base_dir}/cache"
+file_backup_path        "#{base_dir}/backup"
+cache_options({:path => "#{base_dir}/cache/checksums", :skip_expires => true})
