b4403b4e959abab3c0c1a8226ee64a5af640f12f
Sean Fisk <sean@seanfisk.com>
2013-05-22 00:03:35 -0600

    Add Emacs 24.3.

--- cookbooks/macbook_setup/recipes/default.rb
+++ cookbooks/macbook_setup/recipes/default.rb
@@ -24,3 +24,9 @@ dmg_package 'Quicksilver' do
   dmg_name 'Quicksilver 1.0.0'
   action :install
 end
+
+dmg_package 'Emacs' do
+  source 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg'
+  checksum '92b3a6dd0a32b432f45ea925cfa34834c9ac9f7f0384c38775f6760f1e89365a'
+  action :install
+end
