4adde84ca8e840ca78d3d38c57fa213ac213d453
Sean Fisk <sean@seanfisk.com>
2013-05-23 23:50:11 -0600

    Add config files that should work better.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,16 @@
+# Sample knife configuration file
+#
+# For a full list of options, see <http://docs.opscode.com/config_rb_knife.html>.
+
 current_dir = File.dirname(__FILE__)
-log_level                :debug
-log_location             STDOUT
-node_name                "seanfisk"
-client_key               "#{current_dir}/seanfisk.pem"
-validation_client_name   "sean_fisk-validator"
-validation_key           "#{current_dir}/sean_fisk-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/sean_fisk"
-cache_type               'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+
+cache_options({:path => "#{current_dir}/cache/checksums", :skip_expires => true})
+chef_server_url        'https://api.opscode.com/organizations/sean_fisk'
+client_key             "#{current_dir}/seanfisk.pem"
+cookbook_copyright     'Sean Fisk'
+cookbook_email         'sean@seanfisk.com'
+cookbook_license       'apachev2'
+cookbook_path          ["#{current_dir}/../cookbooks"]
+# node_name should correspond to the Opscode hosted chef username. That's because node_name actually specifies the client name, and we want to log in with our "user" client, not our "machine" client.
+node_name              'seanfisk'
+validation_client_name 'sean_fisk-validator'
