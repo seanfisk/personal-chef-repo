fa180056e0e6ddb3e47f7a7c78d88aea71fa36cd
Sean Fisk <sean@seanfisk.com>
2014-02-03 00:26:41 -0500

    Change license and list OS support.

--- cookbooks/macbook_setup/metadata.rb
+++ cookbooks/macbook_setup/metadata.rb
@@ -2,10 +2,11 @@
 name             'macbook_setup'
 maintainer       'Sean Fisk'
 maintainer_email 'sean@seanfisk.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Setup my personal MacBook Pro'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+supports         'mac_os_x'
 depends          'homebrew'
 depends          'dmg'
 depends          'zip'
