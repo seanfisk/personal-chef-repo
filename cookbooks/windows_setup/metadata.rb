401e89f25d9f9eb9d81eb6c4b5143a2b1040de0d
Sean Fisk <sean@seanfisk.com>
2014-05-27 22:48:25 -0400

    Add new windows_setup cookbook.

---
+++ cookbooks/windows_setup/metadata.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8; -*-
+
+name 'windows_setup'
+maintainer 'Sean Fisk'
+maintainer_email 'sean@seanfisk.com'
+license 'Apache 2.0'
+description 'Setup my personal Windows operating system'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+supports 'windows'
+depends 'chocolatey'
