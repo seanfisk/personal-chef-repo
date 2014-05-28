3995f4712ac41e0ca7655ef8e699b299e1af9d12
Sean Fisk <sean@seanfisk.com>
2014-05-28 04:30:51 -0400

    Add guard to execution policy.

--- cookbooks/windows_setup/recipes/default.rb
+++ cookbooks/windows_setup/recipes/default.rb
@@ -27,6 +27,9 @@ node['windows_setup']['packages'].each do |pkg_name|
 end
 
 # Allow running of local PowerShell scripts (default policy is Restricted).
+DESIRED_POLICY = 'RemoteSigned'
 powershell_script 'set execution policy' do
-  code 'Set-ExecutionPolicy RemoteSigned'
+  code "Set-ExecutionPolicy #{DESIRED_POLICY}"
+  guard_interpreter :powershell_script
+  not_if "$(Get-ExecutionPolicy) -eq '#{DESIRED_POLICY}'"
 end
