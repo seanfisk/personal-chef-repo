cea3e051f34e2484f0f3bf19894b4f35365f6096
Sean Fisk <sean@seanfisk.com>
2014-09-29 10:59:04 -0400

    Update XQuartz version.

--- cookbooks/macosx_setup/recipes/default.rb
+++ cookbooks/macosx_setup/recipes/default.rb
@@ -542,12 +542,14 @@ dmg_package 'Wireshark' do
   action :install
 end
 
+XQUARTZ_VERSION = '2.7.7'
 dmg_package 'XQuartz' do
   # Note: XQuartz is installed to /Applications/Utilities/XQuartz.app
-  source 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.5.dmg'
-  checksum '4382ff78cef5630fb6b8cc982da2e5a577d8cc5dddd35a493b50bad2fcf5e34a'
+  source 'http://xquartz.macosforge.org/downloads/SL/'\
+         "XQuartz-#{XQUARTZ_VERSION}.dmg"
+  checksum 'c9b3a373b7fd989331117acb9696fffd6b9ee1a08ba838b02ed751b184005211'
   type 'pkg'
-  volumes_dir 'XQuartz-2.7.5'
+  volumes_dir "XQuartz-#{XQUARTZ_VERSION}"
   package_id 'org.macosforge.xquartz.pkg'
   action :install
 end
