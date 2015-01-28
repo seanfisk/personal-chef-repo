35fbf841ac6b479116481b390b8030acfd840003
Sean Fisk <sean@seanfisk.com>
2015-01-28 01:50:17 -0500

    Use homebrew cookbook's install_formulas recipe.

--- cookbooks/macosx_setup/recipes/default.rb
+++ cookbooks/macosx_setup/recipes/default.rb
@@ -24,7 +24,7 @@ require 'etc'
 require 'uri'
 require 'mixlib/shellout'
 
-# Include homebrew as the default package manager (default is MacPorts).
+# Include Homebrew as the default package manager (default is MacPorts).
 include_recipe 'homebrew'
 
 brew_proc = Mixlib::ShellOut.new('brew', '--prefix')
@@ -641,11 +641,84 @@ end
 # We now also install pyenv through Homebrew, so it's nice to be consistent.
 
 # Install Homebrew packages
-node['macosx_setup']['packages'].each do |pkg_name|
-  package pkg_name do
-    action :install
-  end
-end
+#
+# Notes:
+#
+# - To fix the aria2 build, I ran `brew edit gmp' and added a
+#   `--with-pic' flag. Hopefully I will not have issues in the
+#   future. See here: <https://github.com/mxcl/homebrew/issues/12946>
+# - I prefer ohcount to cloc and sloccount.
+# - Dos2Unix / Unix2Dos <http://waterlan.home.xs4all.nl/dos2unix.html> looks
+#   superior to Tofrodos <http://www.thefreecountry.com/tofrodos/>. But that
+#   was just from a quick look.
+# - ZeroMQ (zmq) is included to speed up IPython installs. It can install a
+#   bundled version to a virtualenv, but it's faster to have a globally built
+#   version.
+# - Install both GraphicMagick and ImageMagick. In generally, I prefer
+#   GraphicsMagick, but ImageMagick has ICO support so we use it for
+#   BetterPlanner.
+# - ImageMagick might already be present on the system (but just 'convert').
+#   I'm not sure if it's just an artifact of an earlier build, but it was on my
+#   Mavericks system before I installed it (again?).
+# - libgit2 is for pygit2 for Powerline.
+# - zpython is also for Powerline.
+# - texinfo is mainly for Sphinx.
+# - pwgen and sf-pwgen are both password generators. pwgen is more generic,
+#   whereas sf-pwgen uses Apple's security framework. We also looked at APG,
+#   but it seems unmaintained.
+# - reattach-to-user-namespace has options to fix launchctl and shim
+#   pbcopy/pbaste. We haven't needed them yet, though.
+#
+node.default['homebrew']['formulas'] = %w(
+  ack
+  aria2
+  astyle
+  coreutils
+  dos2unix
+  doxygen
+  editorconfig
+  fasd
+  gibo
+  git
+  gnu-tar
+  graphicsmagick
+  graphviz
+  htop-osx
+  hub
+  imagemagick
+  libgit2
+  markdown
+  mercurial
+  mobile-shell
+  nmap
+  node
+  ohcount
+  p7zip
+  parallel
+  pstree
+  pwgen
+  pyenv
+  pyenv-virtualenv
+  pyenv-which-ext
+  qpdf
+  rbenv
+  reattach-to-user-namespace
+  ruby-build
+  sf-pwgen
+  ssh-copy-id
+  texinfo
+  the_silver_searcher
+  tmux
+  tree
+  valgrind
+  watch
+  wget
+  xz
+  zmq
+  zpython
+)
+
+include_recipe 'homebrew::install_formulas'
 
 # SkyFonts (http://www.fonts.com/browse/font-tools/skyfonts) allows syncing of
 # fonts between platforms.
